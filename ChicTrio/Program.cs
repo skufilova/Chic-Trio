a25a8beaf373459ec691a66e8e7eb2e2ee905283
Alexandra Kufilova <alexahariwer@gmail.com>
2024-12-16 17:53:30 +0300

    Added Fermat's theorem

---
+++ ChicTrio/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ChicTrio
+{
+    internal class Program
+    {
+        static void CheckFermat(int a, int b, int c, int n)
+        {
+            if (n > 2)
+            {
+                if (Math.Pow(a, n) + Math.Pow(b, n) == Math.Pow(c, n))
+                {
+                    Console.WriteLine($"Существует целое решение для {a}^{n} + {b}^{n} = {c}^{n}");
+                }
+                else
+                {
+                    Console.WriteLine($"Нет целого решения для {a}^{n} + {b}^{n} = {c}^{n}");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            CheckFermat(3, 4, 5, 3);
+        }
+    }
+}
